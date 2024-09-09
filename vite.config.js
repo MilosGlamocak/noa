2b5a1d63f860607d9fe6926644fcaba68ae3d46c
MilosGlamocak <milosglamocak@gmail.com>
2024-09-09 13:31:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'noa',  // Add this line
+  base: '/noa',  // Add this line
   plugins: [react()],
 });
