c94f63a08a67e6dc28ac5ee19a8de09b2fbf79f9
MilosGlamocak <milosglamocak@gmail.com>
2024-09-11 12:08:59 +0200

    styling changes, bg doesnt shift anymore + installed gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import Background from "./components/Background"
-import LogoLoader from "./components/LogoLoader"
-import './App.scss'
-import CustomCursor from "./components/CustomCursor"
-import FirstPage from "./components/FirstPage"
-import SecondPage from "./components/SecondPage"
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
+import Background from "./components/Background";
+import LogoLoader from "./components/LogoLoader";
+import FirstPage from "./components/FirstPage";
+import SecondPage from "./components/SecondPage";
+import './App.scss';
 
 function App() {
+  useEffect(() => {
+    // Add 'no-scroll' class to body to disable scrolling
+    document.body.classList.add("no-scroll");
+
+    // Remove 'no-scroll' class after 2 seconds to enable scrolling
+    const timer = setTimeout(() => {
+      document.body.classList.remove("no-scroll");
+    }, 2000);
+
+    // Cleanup timer on component unmount
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="appCont">
       <FirstPage />
@@ -14,7 +26,7 @@ function App() {
       <LogoLoader />
       <Background />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
