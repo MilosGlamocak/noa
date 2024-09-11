3406f94d38129c6b9b40384e75874b5f01ff53d1
MilosGlamocak <milosglamocak@gmail.com>
2024-09-11 15:18:00 +0200

    topnav mobile test

--- src/components/FirstPage.jsx
+++ src/components/FirstPage.jsx
@@ -1,49 +1,67 @@
-import React, {useRef, useState} from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import '../styles/FirstPage.scss'
 import mouseHover from '/sounds/mouse-hover.mp3'
 import MenuIcon from '@mui/icons-material/Menu';
 
 function FirstPage() {
-
-    let [open, setOpen] = useState(false);
+  let [open, setOpen] = useState(false);
 
   const audioRef = useRef(null);
   const navMobileRef = useRef(null);
   const navRef = useRef(null);
 
-    const handleMouseOver = () => {
-        if (audioRef.current) {
-            audioRef.current.currentTime = 0.205;
-            audioRef.current.volume = 0;
-            audioRef.current.play();
-        }
-    };
-
-    const handleMouseOut = () => {
-        if (audioRef.current) {
-            audioRef.current.pause();
-        }
-    };
-
-    const handleOpenMenu = () => {
-        setOpen(!open)
+  const handleMouseOver = () => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0.205;
+      audioRef.current.volume = 0;
+      audioRef.current.play();
     }
+  };
+
+  const handleMouseOut = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+    }
+  };
+
+  const handleOpenMenu = (e) => {
+    e.stopPropagation(); // Prevent the event from bubbling to the global listener
+    setOpen(true);
+  };
+
+  const handleClickOutside = (e) => {
+    if (navMobileRef.current && !navMobileRef.current.contains(e.target) && navRef.current && !navRef.current.contains(e.target)) {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // Add event listener to close the menu when clicking outside
+    document.body.addEventListener('click', handleClickOutside);
+
+    // Cleanup listener on component unmount
+    return () => {
+      document.body.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className='firstPageCont'>
-        <div className='navContMobile' ref={navMobileRef} onClick={handleOpenMenu} >
-            <div style={{right: open ? '-100%' : '0' }}><MenuIcon/></div>
+      <div className='navContMobile' ref={navMobileRef} onClick={handleOpenMenu} style={{ opacity: open ? '0' : '1' }}>
+        <div style={{ right: open ? '-100%' : '0' }}>
+          <MenuIcon />
         </div>
-        <div className='navCont'>
-            <div style={{left: open ? '0%' : '150%' }}>
-                <audio ref={audioRef} src={mouseHover}></audio>
-                <p onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Home</p>
-                <p onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>About</p>
-                <p onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Services</p>
-                <p onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Contact</p>
-            </div>    
+      </div>
+      <div className='navCont' ref={navRef}>
+        <div style={{ left: open ? '0%' : '150%' }}>
+          <audio ref={audioRef} src={mouseHover}></audio>
+          <p onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Home</p>
+          <p onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>About</p>
+          <p onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Services</p>
+          <p onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Contact</p>
         </div>
-        <div className='logoCont'>
+      </div>
+      <div className='logoCont'>
         <svg width="356" height="135" viewBox="0 0 356 135" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 1H12.2L87 101C87.2 101.2 87.6 101.4 87.8 101.4C88.2 101.4 88.4 101.2 88.8 101C88.8 100.8 89 100.6 89 100.2L88.8 1H93.8V133.2H90.8L6.8 22.6C6.6 22.2 6.2 22 6 22C5.4 22 4.8 22.4 4.8 23.2L4.60001 133.6H0V1Z" fill="white" />
             <path d="M175.99 135C143.39 135 108.39 113.8 108.39 67C108.39 27.6 136.19 0 175.99 0C209.39 0 243.19 23.2 243.19 67.8C243.19 114 208.39 135 175.99 135ZM136.99 17.2C128.19 26.2 123.59 40.2 124.19 56.2C125.59 91.4 148.59 129.6 183.99 129.6C196.79 129.6 207.59 125.4 215.19 117.6C223.79 109 228.19 95.6 227.59 80.2C226.39 44 203.39 5 168.19 5C155.59 5 144.79 9.20001 136.99 17.2Z" fill="white" />
