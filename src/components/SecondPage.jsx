29f7cbcf649490604f848fd5c50d9f5c281a4d93
MilosGlamocak <milosglamocak@gmail.com>
2024-09-12 11:31:49 +0200

    more styling updates

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -5,10 +5,13 @@ import stock1 from '/images/Stock1.jpg'
 function SecondPage() {
   return (
     <div className='secondPageCont'>
-        <p className='description'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aperiam ipsum, numquam, nulla voluptas error architecto quos officiis autem totam natus adipisci. Quibusdam et qui voluptatum sit laudantium. Officia a tempora et rem atque dignissimos, alias rerum velit commodi unde, voluptas obcaecati quo,
-             ea totam beatae vitae iusto distinctio! Quidem rem ipsam alias? <br></br><br />Dolores quos debitis placeat omnis ducimus animi quidem ut ab iusto dolorem, ullam temporibus excepturi tenetur delectus ea! Harum magni ipsam perferendis quidem ducimus voluptate autem est blanditiis,<br /><br></br> libero nostrum et ipsa fugit, iste sed, reiciendis corrupti asperiores minus! Tempora ducimus perspiciatis labore, adipisci corrupti deleniti ea, quam vitae, nostrum assumenda placeat similique nam inventore.
-        </p>
+      <div className='textCont'>
+        <h2>Helping businesses succeed</h2>
+          <p className='description'>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda aperiam ipsum, numquam, nulla voluptas error architecto quos officiis autem totam natus adipisci. Quibusdam et qui voluptatum sit laudantium. Officia a tempora et rem atque dignissimos, alias rerum velit commodi unde, voluptas obcaecati quo,
+              ea totam beatae vitae iusto distinctio! Quidem rem ipsam alias? <br></br><br />Dolores quos debitis placeat omnis ducimus animi quidem ut ab iusto dolorem, ullam temporibus excepturi tenetur delectus ea! Harum magni ipsam perferendis quidem ducimus voluptate autem est blanditiis,<br /><br></br> libero nostrum et ipsa fugit, iste sed, reiciendis corrupti asperiores minus! Tempora ducimus perspiciatis labore, adipisci corrupti deleniti ea, quam vitae, nostrum assumenda placeat similique nam inventore.
+          </p>
+      </div> 
         <img src={stock1} alt=""/>
     </div>
   )
